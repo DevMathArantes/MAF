2a8bb481b5481847264d44bbe99a089c5c42c5f2
math.arantes <matheusoficial1411@gmail.com>
2025-05-27 14:17:07 -0300

    correção

--- functions.js
+++ functions.js
@@ -20,15 +20,14 @@ function solicita(titulo){
     dijuntor('solicita');
     get('solicita').innerHTML=`
         <div class="formularioEsquerda">
-                <button onclick="dijuntor('solicita')" class="fechar">
-                    <img src="Assets/Icons/fechar.png" alt="icone fechar">
-                </button>
-                <h3 class="reescrever">${titulo}...</h3>
-                <p>Preencha o formulário com seus dados</p>
-                <input type="text" placeholder="Nome">
-                <input type="text" placeholder="Endereço">
-                <button class="btnPadrao">Gerar Solicitação</button>
-                <img src="Assets/Images/circuitoBottom.png" alt="imagem de circuitos botton">
-                </div>
+            <button onclick="dijuntor('solicita')" class="fechar">
+                <img src="Assets/Icons/fechar.png" alt="icone fechar">
+            </button>
+            <h3 class="reescrever">${titulo}...</h3>
+            <p>Preencha o formulário com seus dados</p>
+            <input type="text" placeholder="Nome">
+            <input type="text" placeholder="Endereço">
+            <button class="btnPadrao">Gerar Solicitação</button>
+        </div>
     `;
 }
